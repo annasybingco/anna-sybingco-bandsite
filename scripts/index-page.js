c4aef538e69584200ead22be4c8a223d5e57a818
annasybingco <sybingcoanna@gmail.com>
2024-12-19 02:16:27 -0500

    add comment section

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,42 +1,51 @@
 const comments = [
   {
-    name: "Victor",
-    date: "12/19/24",
-    text: "this is a comment",
+    name: "Victor Pinto", 
+    date: "11/02/2023",
+    text: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
   },
   {
-    name: "Victor",
-    date: "12/19/24",
-    text: "this is a comment",
-  }
+    name: "Christina Cabrera", 
+    date: "10/28/2023",
+    text: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+  },
+  {
+    name: "Isaac Tadesse", 
+    date: "10/20/2023",
+    text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+  },
  ]
  
  
  const commentList = document. querySelector(".comment");
- comments.forEach((test) => {
- const commentItem = document.createElement("li");
- commentItem.classList.add("comments__item");
+ comments.forEach((comment) => {
+  const commentItem = document.createElement("div");
+  commentItem.classList.add("comments__item");
 
-   const commentName = document.createElement("p");
+  const commentsInfo = document.createElement("div");
+  commentsInfo.classList.add("comments__info");
+
+  const commentName = document.createElement("p");
   commentName.classList.add("comment__name");
-  commentName.innerText = test.name;
- 
- 
+  commentName.innerText = comment.name;
+
   const commentDate = document.createElement("p");
   commentDate.classList.add("comment__name");
-  commentDate.innerText = test.date;
- 
- 
+  commentDate.innerText = comment.date;
+
   const commentText = document.createElement("p");
   commentText.classList.add("comment__text");
-  commentText.innerText = test.text;
- 
- 
-  commentItem.appendChild(commentName);
-  commentItem.appendChild(commentDate);
+  commentText.innerText = comment.text;
+
+  // Append name and date to the info container
+  commentsInfo.appendChild(commentName);
+  commentsInfo.appendChild(commentDate);
+
+
+  // Append info container and text to the main item
+  commentItem.appendChild(commentsInfo);
   commentItem.appendChild(commentText);
+
+  // Append the item to the list
   commentList.appendChild(commentItem);
- 
- 
- });
- 
+});
